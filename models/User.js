8e03a0a935689de5dfa8827b3ab8880711121834
jordanhughes421 <jordanhughes421@gmail.com>
2024-04-01 09:32:46 -0400

    update

--- models/User.js
+++ models/User.js
@@ -1,12 +1,20 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
-const UserSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
-});
-
+const userSchema = new mongoose.Schema({
+	username: { type: String, required: true, unique: true },
+	email: { type: String, required: true, unique: true },
+	password: { type: String, required: true },
+	firstName: { type: String, required: false }, // Optional based on your requirements
+	lastName: { type: String, required: false }, // Optional based on your requirements
+	companyName: { type: String, required: false }, // For users associated with a company
+	profilePicture: { type: String, required: false }, // Could store a URL to the image
+	role: {
+    	type: mongoose.Schema.Types.ObjectId,
+    	ref: 'Role',
+    	required: true
+	}
+}, { timestamps: true });
 // Hash password before saving
 UserSchema.pre('save', async function(next) {
     if(this.isModified('password')) {
