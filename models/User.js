212528db33776e095e5eb9d389b32b5d9af33770
jordanhughes421 <jordanhughes421@gmail.com>
2024-04-01 09:42:55 -0400

    fixed small bug

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
 	username: { type: String, required: true, unique: true },
 	email: { type: String, required: true, unique: true },
 	password: { type: String, required: true },
