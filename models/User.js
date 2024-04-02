2af2bcf76c1568e15a7708e3d6b303fe4c2a0334
jordanhughes421 <jordanhughes421@gmail.com>
2024-04-02 11:11:16 -0400

    made username non unique, not sure when that would be an issue

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
-	username: { type: String, required: true, unique: true },
+	username: { type: String, required: true },
 	email: { type: String, required: true, unique: true },
 	password: { type: String, required: true },
 	firstName: { type: String, required: false }, // Optional based on your requirements
