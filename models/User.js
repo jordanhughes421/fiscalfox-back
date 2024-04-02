48816f35f5276cb5209b109bb169916c6bf27ed8
jordanhughes421 <jordanhughes421@gmail.com>
2024-04-02 11:58:37 -0400

    made admin default user

--- models/User.js
+++ models/User.js
@@ -13,7 +13,8 @@ const UserSchema = new mongoose.Schema({
 	role: {
     	type: mongoose.Schema.Types.ObjectId,
     	ref: 'Role',
-    	required: false
+    	required: false,
+		default: mongoose.Types.ObjectId('660ab6e4ed0094b616c86de2')
 	}
 }, { timestamps: true });
 // Hash password before saving
